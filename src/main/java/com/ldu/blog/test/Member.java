ea2c672a37c48162338c1c2a5a2e28ffdc4663f0
Moveuk <v_donguk@naver.com>
2021-12-21 21:37:01 +0900

    기본적인 웹 상식 및 postman을 활용한 json 데이터 테스트

---
+++ src/main/java/com/ldu/blog/test/Member.java
@@ -0,0 +1,15 @@
+package com.ldu.blog.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Member {
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+}
