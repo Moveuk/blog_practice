4b20773d74d1e2a77a761b84b13caf82b17bec84
Moveuk <v_donguk@naver.com>
2021-12-22 22:07:38 +0900

    연산 관계 개념 정리 및 Board VO 양방향 매핑

--- src/main/java/com/ldu/blog/model/Board.java
+++ src/main/java/com/ldu/blog/model/Board.java
@@ -1,15 +1,18 @@
 package com.ldu.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -39,9 +42,15 @@ public class Board {
     @ColumnDefault("0")
     private int count; // 조회수
     
-    @ManyToOne // Many(Board) To One(User) - 한명의 유저에 의해서 여러 개의 게시글 작성 가능.
+    @ManyToOne(fetch = FetchType.EAGER) // Many(Board) To One(User) - 한명의 유저에 의해서 여러 개의 게시글 작성 가능.
+    // ManyToOne의 기본 fetch 전략은 각 하나만을 가져오므로 EAGER이다.
     @JoinColumn(name= "userId") // 테이블에는 userId 로 저장되도록 설정
     private User user; // 글쓴이 - DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다.
+    
+    // mappedBy - 연관관계의 주인이 아니므로 DB에 컬럼을 만들지 않도록 해줌.
+    @OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // 여기서 board는 Reply VO에 field로 사용된 변수명이다.
+    // OneToMany의 기본 fetch 전략은 LAZY이다.
+    private List<Reply> reply; // JPA가 해당 게시글에 연결된 댓글을 모두 가져오면 여러개이므로 컬렉션으로 받아야함.
     
     @CreationTimestamp
     private Timestamp createDate;
