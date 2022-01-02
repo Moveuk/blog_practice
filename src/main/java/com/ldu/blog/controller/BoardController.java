2cfb2331b000f565ec2a2cc92dfa971a2dbf8053
Moveuk <v_donguk@naver.com>
2022-01-02 14:16:24 +0900

    로그인 설명 수정

--- src/main/java/com/ldu/blog/controller/BoardController.java
+++ src/main/java/com/ldu/blog/controller/BoardController.java
@@ -1,17 +1,13 @@
 package com.ldu.blog.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.ldu.blog.config.auth.PrincipalDetail;
 
 @Controller
 public class BoardController {
 
 	@GetMapping({"","/"}) // 배열로 두가지 매핑 가능.
-	public String index(@AuthenticationPrincipal PrincipalDetail principal) {
-		System.out.println("로그인 사용자 아이디 : "+principal.getUsername());
+	public String index() {
 		return "index"; // index.jsp로 이동
 	}
 }
