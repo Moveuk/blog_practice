48338953ccd6366d2fc1784dd3d5dfe8db85a07f
Moveuk <v_donguk@naver.com>
2022-01-04 00:24:05 +0900

    글 삭제하기 기능 추가

--- src/main/java/com/ldu/blog/service/BoardService.java
+++ src/main/java/com/ldu/blog/service/BoardService.java
@@ -1,6 +1,7 @@
 package com.ldu.blog.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -30,13 +31,25 @@ public class BoardService {
 		return -1;
 	}
 
+	@Transactional(readOnly = true)
 	public Page<Board> 글목록(Pageable pageable) {
 		return boardRepository.findAll(pageable);
 	}
 
+	@Transactional(readOnly = true)
 	public Board 글상세보기(int id) {
-		return boardRepository.findById(id).orElseThrow(()->{
+		return boardRepository.findById(id).orElseThrow(() -> {
 			return new IllegalArgumentException("글 상세보기 실패 : 아이디를 찾을 수 없습니다.");
 		});
+	}
+
+	@Transactional
+	public int 글삭제하기(int id) {
+		try {
+			boardRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			return -1;
+		}
+		return 1;
 	}
 }
