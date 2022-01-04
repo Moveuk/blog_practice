f6e1288f1f6760d1a1e4382518ed2635ec1d0a1f
Moveuk <v_donguk@naver.com>
2022-01-04 12:32:59 +0900

    스프링 개념 정리

--- src/main/java/com/ldu/blog/config/SecurityConfig.java
+++ src/main/java/com/ldu/blog/config/SecurityConfig.java
@@ -17,18 +17,18 @@ import com.ldu.blog.config.auth.PrincipalDetailService;
 
 // 빈 등록 : 스프링 컨테이너에서 객체를 관리할 수 있게 설정
 @Configuration // 빈등록 (IoC 관리)
-@EnableWebSecurity // 시큐리티 필터 추가(Default로 필터됨) -> 필터에 대한 설정을 이 클래스에서 정리함.
+@EnableWebSecurity(debug = true) // 시큐리티 필터 추가(Default로 필터됨) -> 필터에 대한 설정을 이 클래스에서 정리함.
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소로 접근을 하면 권한 및 인증을 미리 체크하겠다는 뜻.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private PrincipalDetailService principalDetailService;
-	
+
 	@Bean // IoC 등록 : return 값을 스프링이 관리하도록 만듬. - 해쉬화 하는 함수를 리턴함.
 	public BCryptPasswordEncoder encodePWD() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	// 시큐리티가 대신 로그인시 password를 가로챌때 해쉬할 방식을 정해줌.
 	// DB의 해쉬화된 비밀번호와 비교하려면 같은 해쉬 함수로 변경해주어야 하기 때문.
 	@Override
@@ -50,8 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.permitAll() // 상위주소 요청은 인증절차 없이 사용을 허가
 				.anyRequest() // 다른 요청은
 				.authenticated() // 인가 받아야함.
-			.and()
-				.formLogin() // 위에서 다른 요청은 인가를 받아야 하므로
+				.and().formLogin() // 위에서 다른 요청은 인가를 받아야 하므로
 				.loginPage("/auth/loginForm") // 로그인 페이지로 보냄
 				.loginProcessingUrl("/auth/loginProc") // 스프링 시큐리티가 해당 주소로 요청오는 로그인을 가로채어 대신 로그인 과정을 진행함.
 				.defaultSuccessUrl("/"); // 로그인에 성공하게 되면 이동하게 되는 페이지를 적어줌.
